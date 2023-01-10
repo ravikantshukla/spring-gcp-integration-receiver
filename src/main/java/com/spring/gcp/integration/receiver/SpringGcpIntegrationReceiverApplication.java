c4885565efd1ccccabed0567e0521080e7963f6a
Ravi Kant Shukla <ravikanth.s@sleepcountry.ca>
2023-01-10 18:24:20 +0530

    Initial commit

---
+++ src/main/java/com/spring/gcp/integration/receiver/SpringGcpIntegrationReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gcp.integration.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGcpIntegrationReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGcpIntegrationReceiverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/gcp/integration/receiver/SpringGcpIntegrationReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gcp.integration.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGcpIntegrationReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
